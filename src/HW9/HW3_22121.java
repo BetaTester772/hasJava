9f6dab16bcf3ff1405f91103322bae591519cc91
Hoseong Ahn <hoseong8115@naver.com>
2023-06-12 14:38:35 +0900

    #HW9 2, 3번 분리

--- src/HW9/HW3_22121.java
+++ src/HW9/HW3_22121.java
@@ -2,7 +2,7 @@ package HW9;
 
 import java.util.Scanner;
 
-public class HW2_22121 {
+public class HW3_22121 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -18,7 +18,13 @@ public class HW2_22121 {
     }
 
     public static void printTimes(int n) {
+        if (n <= 9)
             for (int i = 1; i <= 9; i++)
                 System.out.println(n + " * " + i + " = " + n * i);
+
+        else
+            for (int i = 1; i <= n; i++)
+                System.out.println(n + " * " + i + " = " + n * i);
+
     }
 }
