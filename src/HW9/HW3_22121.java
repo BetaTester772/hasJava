705b909bcfbd262228cb30ff54f3800f94c97874
Hoseong Ahn <hoseong8115@naver.com>
2023-06-12 14:58:03 +0900

    edit HW9 3번

--- src/HW9/HW3_22121.java
+++ src/HW9/HW3_22121.java
@@ -5,16 +5,8 @@ import java.util.Scanner;
 //HW3_안호성
 public class HW3_22121 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int x = getInput();
 
-        int x;
-
-        do {
-            System.out.print("정수를 입력하세요: ");
-            x = sc.nextInt();
-            if (x <= 0)
-                System.out.println("잘못된 입력입니다. 다시 입력하세요.");
-        } while (x <= 0);
         printTimes(x);
     }
 
@@ -26,5 +18,17 @@ public class HW3_22121 {
         for (int i = 1; i <= num; i++)
             System.out.println(n + " * " + i + " = " + n * i);
 
+    }
+
+    public static int getInput() {
+        int x;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("정수를 입력하세요: ");
+            x = sc.nextInt();
+            if (x <= 0)
+                System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+        } while (x <= 0);
+        return x;
     }
 }
