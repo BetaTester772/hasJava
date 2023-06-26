efd0d87deabc2ca77695b05df21729c82d9d876a
Hoseong Ahn <hoseong8115@naver.com>
2023-06-26 14:58:00 +0900

    HW11

---
+++ src/HW11/HW2_22121.java
@@ -0,0 +1,62 @@
+package HW11;
+
+// HW2_안호성
+public class HW2_22121 {
+    public static void main(String[] args) {
+        Date d = new Date();
+
+        d.setYear(2006);
+        d.setMonth(12);
+        d.setDay(28);
+        System.out.println("생일은 " + d.toString() + "입니다.");
+
+        d.setYear(d.getYear() + 20);
+        System.out.println("성년일은 " + d.toString() + "입니다.");
+
+    }
+}
+
+class Date {
+    private int year;
+    private int month;
+    private int day;
+
+    public Date() {
+        year = 1900;
+        month = 1;
+        day = 1;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public String toString() {
+        String rslt = "";
+        rslt += getYear() + "년 ";
+        rslt += getMonth() + "월 ";
+        rslt += getDay() + "일";
+
+        return rslt;
+    }
+}
